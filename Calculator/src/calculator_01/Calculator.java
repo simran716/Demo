d1e21cad52d3c0e25e2f0166e11134184d3ef782
Simran gill <64963386+simran716@users.noreply.github.com>
2022-08-03 23:03:57 +0530

    Add files via upload
    First commit

---
+++ Calculator/src/calculator_01/Calculator.java
@@ -0,0 +1,37 @@
+package calculator_01;
+
+import java.util.Scanner;
+
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter first number:- ");
+		double num1=sc.nextDouble();
+		System.out.println("Enter second number:- ");
+		double num2=sc.nextDouble();
+		System.out.println("Enter the operator (+,-,*,/)");
+		char op = sc.next().charAt(0);
+		double result =0;
+		switch(op){
+		case '+':
+			result=num1+num2;
+			break;
+		case '-':
+			result=num1-num2;
+			break;
+		case '*':
+			result=num1*num2;
+			break;
+		case '/':
+			result=num1/num2;
+			break;
+		default :
+			System.out.println("You enter wrong input.");
+		}
+		System.out.println("Final result is:-");
+		System.out.println();
+		System.out.println(num1+" "+op+" "+num2+" ="+result);
+
+	}
+
+}
