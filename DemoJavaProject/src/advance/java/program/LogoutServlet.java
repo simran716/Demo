b7dad4c98063ca71d8ba92233871fa212262d423
Simran gill <64963386+simran716@users.noreply.github.com>
2022-08-22 16:27:06 +0530

    Add files via upload
    First commit

---
+++ DemoJavaProject/src/advance/java/program/LogoutServlet.java
@@ -0,0 +1,22 @@
+package advance.java.program;
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;  
+public class LogoutServlet extends HttpServlet {  
+        protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+                                throws ServletException, IOException {  
+              
+            PrintWriter out=response.getWriter();  
+              
+            
+            out.println("You are successfully logged out!");  
+              
+            out.close();  
+    }  
+}  
